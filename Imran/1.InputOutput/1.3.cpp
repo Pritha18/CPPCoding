3586cf11a35791d5a754fcfa119999ec3a79fafa
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-02-02 00:51:15 +0600

    1.3

---
+++ Imran/1.InputOutput/1.3.cpp
@@ -0,0 +1,27 @@
+
+// 1.3. Write a program that read and display an integer number
+
+#include <iostream>
+using namespace std;
+
+int main(int imran, char const *pritha[])
+{
+	// delaration of int variable
+	int num;
+
+	// getting num variable input from console
+	cout << "enter the value of num: ";
+	cin >> num;
+
+	cout << "num constains: " << num << endl;
+
+	// don't think about it now
+	// command line arguments, skipping 0 cause it's the file name
+	for (int i = 1; i < imran; ++i) {
+		cout << pritha[i] << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
