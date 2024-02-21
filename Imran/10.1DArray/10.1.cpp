559d53c319e4ee826bd86b87e80814c56d8792a9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-02-22 02:36:02 +0600

    array intro

---
+++ Imran/10.1DArray/10.1.cpp
@@ -0,0 +1,33 @@
+
+// 10.1. Write a program that read and display an array
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	// declaring a single variable
+	int imran;
+
+	// array's will hold only one type
+
+	// declaring an int array
+	// this will hold only int values
+	// this is holding 5 int varibales under pritha
+	int pritha[5];
+
+	printha[0];
+	printha[1];
+	printha[2];
+	printha[3];
+	printha[4];
+
+	// this will hold only double values
+	// this is holding 10 double varibales under promi
+	double promi[10];
+
+	// this will hold only char values
+	char sakib[256];
+
+	return 0;
+}
