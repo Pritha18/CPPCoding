5b8f38e711587ca7d41bfa69fbcb27e24e0efa38
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-05-14 00:51:17 +0600

    loop theory

---
+++ Imran/6.Series/For/6.2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(int argc, char const *argv[])
+{
+
+
+	return 0;
+}
+
+/*
+	it will not run if the condition is false
+	while (condition) {
+		statement
+	}
+	
+	it will run once even if condition is false
+	do {
+		statement
+	} while (condition)
+
+	
+	it will not run if the condition is false
+	for (initial value; condition; increment or decrement) {
+		statement
+	}
+
+*/
--- Imran/6.Series/temp.cpp
+++ Imran/6.Series/temp.cpp
@@ -41,3 +41,90 @@ void insertion_sort(int ar[], int size)
 
 	S(n) = n + 4
 */
+
+
+/*
+	merge
+
+	T(n) = 3 + n ( 1 + 1 + 1 + 1 + 1 + 1 )+ n (1 + 1 + 1) + n (1 + 1 + 1)
+	     = 3 + 6n + 3n + 3n
+	     = 12n + 3
+
+
+*/
+
+void merge(int left[],int size_left,int right[], int size_right, int ar[])
+{
+	int i = 0, j = 0, k = 0; // 3
+
+	while(i < size_left && j < size_right) //  n
+	{
+		if (left[i] <= right[j]) // 1
+		{
+			ar[k] = left[i]; // 1
+
+			i++; // 1
+		}
+		else
+		{
+			ar[k] = right[j]; // 1
+
+			j++; // 1
+		}
+
+		k++; // 1
+	}
+
+	while(i < size_left) // n
+	{
+		ar[k] = left[i]; // 1
+		
+		i++; // 1
+		k++; // 1
+	}
+
+	while(j < size_right) // n
+	{
+		ar[k] = right[j]; // 1
+
+		j++; // 1
+		k++; // 1
+	}
+}
+
+
+/*
+	T (n) = 5 + n / 2 + n / 2 + T(n / 2) + T (n / 2) 
+
+	T (n / 2) = 5 + n / 4 + n / 4 + T (n / 4) + T (n / 4)
+
+	T (n / 2^k) = 
+
+*/
+
+void merge_sort(int ar[], int size)
+{
+	if (size < 2) // 1
+	{
+		return; // 1
+	}
+
+	int mid = size/2; // 1
+
+	int left[mid],right[size-mid]; // 2
+
+	for (int i = 0; i < mid; ++i) // n / 2
+	{
+		left[i] = ar[i]; // 1
+	}
+
+	for (int i = mid; i < size; ++i) // n / 2
+	{
+		right[i-mid] = ar[i]; // 1
+	}
+
+	merge_sort(left,mid); // T (n / 2)
+	merge_sort(right,size-mid); // T (n / 2)
+
+	merge(left,mid,right,size-mid,ar);
+}
